675cba58a4fcea7effe77e208bea5b7ebfbe8419
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2025-07-28 11:16:12 +0100

    Solve 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min)
+        || !Number.isInteger(max)
+        || min < 0
+        || max < 0
+    ) {
+        return "ERROR"
+    }
 
+    if (max < min) {
+        const temp = max;
+        max = min;
+        min = temp;
+    }
+
+    // Sum of a range of numbers can be found with (n/2) * (first + last)
+    return ((max - min + 1)/2) * (min + max);
 };
 
 // Do not edit below this line
