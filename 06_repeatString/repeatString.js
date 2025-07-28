52b964361e768fa85c2c898362d3dac4809c381c
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2025-07-28 10:54:56 +0100

    Solve 06 repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
+    
+    outputStr = "";
+    for (let i = 0; i < n; i++) {
+        outputStr += str;
+    }
+    return outputStr;
 };
 
 // Do not edit below this line
