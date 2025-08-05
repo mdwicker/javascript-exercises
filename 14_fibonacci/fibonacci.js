679435d34672697789daeb68f935e6de162b9199
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2025-08-05 13:56:48 +0100

    Solve 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if (n < 0) {
+        return "OOPS";
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+
+    let prev = 0;
+    let curr = 1;
+
+    for (let i = 0; i < n - 1; i++) {
+        let temp = curr + prev;
+        prev = curr;
+        curr = temp;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
