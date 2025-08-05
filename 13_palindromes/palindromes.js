de4d9149a9d7f1afb8703eb9efc5d1aa01337909
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2025-08-05 13:43:34 +0100

    Solve 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const nonWord = /\W/gi;
+    const cleanedStr = str.toLowerCase().replaceAll(nonWord, "");
+    return cleanedStr.split("").reverse().join("") === cleanedStr;
 };
 
 // Do not edit below this line
