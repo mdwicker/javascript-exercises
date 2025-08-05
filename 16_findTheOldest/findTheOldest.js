0706242bcb50475b1c3b200259a0d8694ee7402a
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2025-08-05 14:08:28 +0100

    Solve 16 findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        const currentYear = new Date().getFullYear();
+        if (!oldest.yearOfDeath) {
+            oldest.yearOfDeath = currentYear;
+        }
 
+        if (!current.yearOfDeath) {
+            current.yearOfDeath = currentYear;
+        }
+
+        let currentAge = current.yearOfDeath - current.yearOfBirth;
+        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+
+        if (currentAge > oldestAge) {
+            return current;
+        }
+
+        return oldest;
+    })
 };
 
 // Do not edit below this line
