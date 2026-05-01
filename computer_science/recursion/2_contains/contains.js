9290082c3ec026c61023ce72a91f8d690ffd59dd
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2026-05-01 21:02:05 +0100

    solved contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,13 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  for (const x of Object.values(obj)) {
+    if (typeof x === "object" && !Array.isArray(x) && x !== null) {
+      if (contains(x, value)) return true;
+    } else if (x === value) {
+      return true;
+    } else if (Number.isNaN(value) && Number.isNaN(x)) return true;
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
