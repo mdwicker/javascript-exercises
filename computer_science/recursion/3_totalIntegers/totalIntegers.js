fa363487e03780b6702266ea3ede4893d5ea9a3f
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2026-05-02 13:57:07 +0100

    solve totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,22 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (container) {
+  let total = 0;
+
+  if (!isObject(container)) return undefined;
+
+  for (const item of Object.values(container)) {
+    if (Number.isInteger(item)) {
+      total += 1;
+    } else if (isObject(item)) {
+      total += totalIntegers(item);
+    }
+  }
+
+  return total;
 };
-  
+
+function isObject(obj) {
+  return obj !== null && typeof obj === "object";
+}
+
 // Do not edit below this line
 module.exports = totalIntegers;
