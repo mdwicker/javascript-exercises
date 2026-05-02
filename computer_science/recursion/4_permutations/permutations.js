452646671f8e46ea1d17ff4bcd9b8acbd0b34357
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2026-05-02 15:17:12 +0100

    solve permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,19 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (arr.length <= 1) {
+    return [arr];
+  }
+
+  const perms = [];
+  const curr = arr[0];
+
+  for (const perm of permutations(arr.slice(1))) {
+    // for each permutation, add the current number in each possible slot
+    for (let i = 0; i < arr.length; i++) {
+      perms.push([...perm.slice(0, i), curr, ...perm.slice(i)]);
+    }
+  }
+  return perms;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
