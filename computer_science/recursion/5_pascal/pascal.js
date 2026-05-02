ac3e0ef6989712567979baf642b6d016a24786de
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2026-05-02 15:24:04 +0100

    solve pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,14 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+
+  const line = [0, ...pascal(n - 1)];
+  for (let i = 0; i < line.length - 1; i++) {
+    line[i] += line[i + 1];
+  }
+  return line;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
